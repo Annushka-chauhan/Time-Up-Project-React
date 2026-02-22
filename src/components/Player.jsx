738415e667f0e25510d62f783bc1e6e83242600a
anushka chauhan <anushkachauhanannu@gmail.com>
2026-02-22 15:09:32 +0530

    updated Code

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,29 @@
-import {useState} from 'react';
+import {useState,useRef } from 'react';
 export default function Player() {
-
-   const [enteredPlayerName, setEnteredPlayer] = useState(''); 
-   const [submitted, setSubmitted] = useState(false);
-   function handleChange(event){
-    //we are setting here false as when we start typing again it must not change with each key stroke but is updaed wwith the unknown entity 
-    setSubmitted(false);
-    setEnteredPlayer(event.target.value)
-   }
+  const playerName = useRef();
+   const [enteredPlayerName, setEnteredPlayer] = useState(null); 
+  // const [submitted, setSubmitted] = useState(false);
+  //  function handleChange(event){
+  //   //we are setting here false as when we start typing again it must not change with each key stroke but is updaed wwith the unknown entity 
+  //   setSubmitted(false);
+  //   setEnteredPlayer(event.target.value)
+  //  }
    function handleClick(){
-   setSubmitted(true);
+  //  setSubmitted(true);
+ setEnteredPlayer(playerName.current.value);
+ playerName.current.value = ''; 
    }
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName: 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ? enteredPlayerName: 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange= {handleChange} value={enteredPlayerName} />
+        <input
+        // This input component here is connected to the ref 
+         ref={playerName} 
+         type="text"
+         />
+         {/* onChange= {handleChange} 
+         value={enteredPlayerName}  */}
         <button onClick = {handleClick}>Set Name</button>
       </p>
     </section>
