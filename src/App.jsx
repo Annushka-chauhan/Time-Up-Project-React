267bb8d150a235f5d7baad4bb578baa24d6938fa
anushka chauhan <anushkachauhanannu@gmail.com>
2026-02-22 15:32:04 +0530

    New Updated File

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        {/* Because 1 is a number, and JSX attributes without {} are always treated as strings. */}
+        <TimerChallenge title="Easy" targetTime ={1}/>
+         <TimerChallenge title="Not Easy" targetTime ={5}/>
+          <TimerChallenge title="Getting Tough" targetTime ={10}/>
+           <TimerChallenge title="Pros Only " targetTime ={15}/>
+      </div>
     </>
   );
 }
