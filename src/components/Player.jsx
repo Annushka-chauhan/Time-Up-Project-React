f67955a6eba5f0314a2b93667ec944833e2b2bda
anushka chauhan <anushkachauhanannu@gmail.com>
2026-02-22 12:30:19 +0530

    Updated file

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import {useState} from 'react';
 export default function Player() {
+
+   const [enteredPlayerName, setEnteredPlayer] = useState(''); 
+   const [submitted, setSubmitted] = useState(false);
+   function handleChange(event){
+    //we are setting here false as when we start typing again it must not change with each key stroke but is updaed wwith the unknown entity 
+    setSubmitted(false);
+    setEnteredPlayer(event.target.value)
+   }
+   function handleClick(){
+   setSubmitted(true);
+   }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName: 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange= {handleChange} value={enteredPlayerName} />
+        <button onClick = {handleClick}>Set Name</button>
       </p>
     </section>
   );
